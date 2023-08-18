3196f8e1f325d2936cde7eae357bafcd8d059cbb
jo0oy <rkfkrh07@naver.com>
2023-08-18 11:51:57 +0900

    [당구 연습] 거리 제곱근 저장 로직에 우선순의 큐를 사용하도록 코드 수정 업데이트

--- src/programmers/당구연습.java
+++ src/programmers/당구연습.java
@@ -18,33 +18,33 @@ public class 당구연습 {
         int idx = 0;
         for(int[] ball : balls) {
 
-            // 해당 공을 맞출 수 있는 경우의 거리 제곱근을 담을 list
-            List<Integer> disList = new ArrayList<>();
+            // 해당 공을 맞출 수 있는 경우의 거리 제곱근을 담을 우선순위 큐
+            PriorityQueue<Integer> distQueue = new PriorityQueue<>();
 
             // 상 대칭
             if(!(startX == ball[0] && startY < ball[1])) {
-                disList.add(calculateDistance(startX, startY, ball[0], (n - ball[1]) * 2 + ball[1]));
+                distQueue.add(calculateDistance(startX, startY, ball[0], (n - ball[1]) * 2 + ball[1]));
             }
 
             // 하 대칭
             if(!(startX == ball[0] && startY > ball[1])) {
-                disList.add(calculateDistance(startX, startY, ball[0], (-1) * ball[1]));
+                distQueue.add(calculateDistance(startX, startY, ball[0], (-1) * ball[1]));
             }
 
             // 좌 대칭
             if(!(startY == ball[1] && startX > ball[0])) {
-                disList.add(calculateDistance(startX, startY, (-1) * ball[0], ball[1]));
+                distQueue.add(calculateDistance(startX, startY, (-1) * ball[0], ball[1]));
             }
 
             // 우 대칭
             if(!(startY == ball[1] && startX < ball[0])) {
-                disList.add(calculateDistance(startX, startY, (m - ball[0]) * 2 + ball[0], ball[1]));
+                distQueue.add(calculateDistance(startX, startY, (m - ball[0]) * 2 + ball[0], ball[1]));
             }
 
-            // 오름차순 정렬 --> 최솟값 구하기 위해
-            Collections.sort(disList);
+            if(distQueue.isEmpty()) continue;
 
-            answer[idx++] = disList.get(0);
+            // 기본 우선순위 큐 --> 오름차순 정렬
+            answer[idx++] = distQueue.poll();
         }
 
         return answer;
